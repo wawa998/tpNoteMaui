21ff41d7128f346929dfb7b6da91adb050794241
couvidoux <gwendal.couvidoux@etu.unilim.fr>
2024-03-31 00:51:20 +0100

    ajout visuel image sélectionné

--- applicationMauiCouvidoux/obj/Debug/net8.0-android/applicationMauiCouvidoux.AssemblyInfo.cs
+++ applicationMauiCouvidoux/obj/Debug/net8.0-android/applicationMauiCouvidoux.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("applicationMauiCouvidoux")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7f6028b3c0a93a701ca03c199c2df052f6d06c44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6a9a805b78f7c7fbbb3f9c5a1449f2badaabf495")]
 [assembly: System.Reflection.AssemblyProductAttribute("applicationMauiCouvidoux")]
 [assembly: System.Reflection.AssemblyTitleAttribute("applicationMauiCouvidoux")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
